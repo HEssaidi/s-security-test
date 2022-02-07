26c41598963b40d958b32a01c1c3ed5ea14d9977
HEssaidi <essaidihajar50@gmail.com>
2022-02-07 13:48:00 +0100

    checkout jdbc dependency

--- src/main/java/com/example/googleoauthtest/config/SecurityConfiguration.java
+++ src/main/java/com/example/googleoauthtest/config/SecurityConfiguration.java
@@ -1,17 +1,25 @@
 package com.example.googleoauthtest.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -24,14 +32,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("blah")
-                .password("blah")
+        auth.jdbcAuthentication()
+        .dataSource(dataSource)
+        .withDefaultSchema()
+        .withUser(
+                User.withUsername("user")
+                .password("pass")
                 .roles("USER")
-                .and()
-                .withUser("foo")
-                .password("foo")
-                .roles("ADMIN");
+        )
+                .withUser(
+                        User.withUsername("admin")
+                                .password("pass")
+                                .roles("ADMIN")
+                );
     }
 
     @Bean
