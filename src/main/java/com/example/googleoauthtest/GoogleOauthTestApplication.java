3ab845c78a92f83154231bd8702277fb6c8be9be
HEssaidi <essaidihajar50@gmail.com>
2022-02-08 17:45:50 +0100

    google OAuth

--- src/main/java/com/example/googleoauthtest/GoogleOauthTestApplication.java
+++ src/main/java/com/example/googleoauthtest/GoogleOauthTestApplication.java
@@ -2,10 +2,11 @@ package com.example.googleoauthtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class GoogleOauthTestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GoogleOauthTestApplication.class, args);
 	}
